8499ed1f99378a2b008be56a6f1002cffc53c44c
Marcin Krystianc <marcin.krystianc@gmail.com>
2025-01-21 18:22:14 +0100

    diff noise

--- scripts/perftests/testCases/LargeAppCPM64-142722b.ps1
+++ scripts/perftests/testCases/LargeAppCPM64-142722b.ps1
@@ -24,10 +24,5 @@ ForEach ($version In $versions) {
 	Invoke-WebRequest -Uri "$url" -OutFile ("$version" + ".tar.gz")
 	New-Item -Name "$version" -ItemType "Directory"
 	. tar xfz ("$version" + ".tar.gz") --directory "$version"
-	. ".\$version\dotnet" --info
-	$env:DOTNET_ROOT = (Resolve-Path ".\$version").Path
-	$env:PATH = (Resolve-Path ".\$version\").Path + ";$env:PATH"
-	echo $env:path
-	dir env:
 	. "$PSScriptRoot\..\RunPerformanceTests.ps1" -nugetClientFilePath "$version\dotnet" -solutionFilePath $solutionFilePath -resultsFilePath $resultsFilePath -iterationCount 1 -staticGraphRestore
 }
